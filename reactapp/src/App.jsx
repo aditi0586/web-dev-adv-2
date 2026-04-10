02b170059b6caf89e0cb0307438d4470e1e26754
Aditi Pandey <pandayaditi05@gmail.com>
2026-04-10 19:33:52 +0530

    Added new changes

---
+++ reactapp/src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import './App.css'
+import Article from './components/Article'
+import Menu from './components/menu'
+function App() {
+
+  return (
+    <>
+      <Menu/>
+      <Article/>
+    </>
+  )
+}
+
+export default App
